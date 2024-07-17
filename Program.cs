961168d861a6a485b8a603b357516e3fd6178803
mein9 <mateo0221@gmail.com>
2024-07-17 14:18:19 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace ApplicationsWeek3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Time t1 = new Time();
+            Console.WriteLine(t1.ToString());
+
+            Time t2 = new Time(1, 19);
+            Console.WriteLine(t2);
+
+            Time t3 = new(3, 50, 23);
+            t3.WriteTime();
+            Time.WriteTime(t3);
+        }
+
+        
+            
+        
+
+
+       
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,43 @@
+﻿
+namespace ApplicationsWeek3
+{
+    internal class Time
+    {
+        public int hour, minute;
+        public double second;
+
+        public Time()
+        {
+            hour = 1;
+            minute = 2;
+            second = 3.0;
+        }
+        public Time(int hour, int minute)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            
+        }
+        public Time(int hour, int minute, double second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+        public override string ToString()
+        {
+            return this.hour + ":" + this.minute + ":" + this.second;
+        }
+
+        public void WriteTime()
+        {
+            Console.WriteLine(hour + ":" + minute + ":" + second);
+            
+        }
+        public static void WriteTime(Time t)
+        {
+            Console.WriteLine(t.hour + ":" + t.minute + ":" + t.second);
+
+        }
+    }
+}
